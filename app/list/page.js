0060867ce41f87507b58449018305ec7a0cf81ac
1three <1_three@naver.com>
2023-06-26 19:23:27 +0900

    feat: array, map, useState를 이용한 +, - 버튼 생성

--- app/list/page.js
+++ app/list/page.js
@@ -4,7 +4,7 @@ import { useState } from "react"
 
 export default function List() {
     let 상품 = ['tomato', 'pasta', 'coconut'] //[array]
-    let [수량, 수량변경] = useState(0)
+    let [수량, 수량변경] = useState([0, 0, 0]) //[array]
     // state 변경 시
     // 1. 자동 재렌더링
     // 2. state 변경함수() 사용 필수
@@ -17,12 +17,17 @@ export default function List() {
               <div className="food" key={i}>
                 <img src={`/food${i}.png`} className="food-img" ></img>
                 <h4>{ name } 100,000</h4>
-                <span> { 수량 } </span> {/* span: 줄바꿈 없는 div 태그 */}
-                <button onClick={() => { 
-                  수량변경(수량+1)
-                }}>
-                +
-                </button>
+                <button onClick={() => {
+                  let copy = [...수량] // 독립적 [array] 사본 생성
+                  copy[i]--
+                  수량변경(copy)
+                }}>-</button>
+                <span> { 수량[i] } </span> {/* span: 줄바꿈 없는 div 태그 */}
+                <button onClick={() => {
+                  let copy = [...수량]
+                  copy[i]++
+                  수량변경(copy)
+                }}>+</button>
               </div>
             )
           })
